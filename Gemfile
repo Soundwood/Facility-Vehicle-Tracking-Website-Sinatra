560a8fa87a0bc6096f35c05bd70f67a8c5842a71
Soundwood <j.benhogan@gmail.com>
2020-08-04 16:19:35 -0500

    created user db and login/out functionlity

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'shotgun'
 gem 'pry'
 gem 'bcrypt'
 gem 'tux'
+gem 'rack_session_access'
 
 group :test do
   gem 'rspec'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "purplepandageneral"
+  set :views, Proc.new { File.join(root, "../views/") }
+  
+  configure do
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "purplepandageneral"
+  end
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
